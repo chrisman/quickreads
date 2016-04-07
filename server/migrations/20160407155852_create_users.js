c20aac04281715caa6f357474a4fa7691d66bc15
Christopher Brown <christopher.p.brown@gmail.com>
2016-04-07 16:02:33 -0600

    :zap: knex

---
+++ server/migrations/20160407155852_create_users.js
@@ -0,0 +1,14 @@
+
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('users', function(t){
+    t.increments();
+    t.string('uid');
+    t.string('email');
+    t.string('password');
+    t.timestamps();
+  });  
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTable('users');
+};
